56a6d0293b0614ba7aaee54996cc8004ec08c2d3
tomasgrau2 <t.grau@alumno.um.edu.ar>
2023-10-24 12:34:25 -0300

    Ejemplo

--- main.py
+++ main.py
@@ -1,62 +1,53 @@
-from fastapi import FastAPI
-from models.product import Product
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-products = [
-    {
-        "id":1,
-        "name": "Product 1",
-        "price": 20,
-        "stock": 10
-    },
-    {
-        "id":2,
-        "name": "Product 2",
-        "price": 20,
-        "stock": 10
-    }
-]
 
-### GETS
-
-@app.get('/')
-def message():
-    return "Hello World"
-
-@app.get('/products')
-def get_products():
-    return products
-
-@app.get('/products/{id}')
-def get_product(id: int):
-    return list(filter(lambda item: item['id'] == id, products))
-
-@app.get('/products/')
-def get_products_by_stock(stock: int, price: float):
-    return list(filter(lambda item: item['stock'] == stock and item['price'] == price, products))
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-### POSTS
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
 
-@app.post('/products/')
-def create_product(product: Product):
-    products.append(product)
-    return products
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
-@app.put('/products/')
-def update_product(id: int, product: Product):
-    for index, item in enumerate(products):
-        if item['id'] == id:
-            products[index]['name'] = product.name
-            products[index]['stock'] = product.stock
-            products[index]['price'] = product.price
-    return products
 
-@app.delete('/products/{id}')
-def delete_product(id: int):
-    for item in products:
-        if item['id'] == id:
-            products.remove(item)
-    return products
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
